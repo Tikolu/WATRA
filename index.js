3a4789129d6fe70118e09298f8a17197b6f0225d
Wiktor Owczarek <wiktor.owczarek@zhp.ie>
2025-03-04 22:51:27 +0000

    Minor changes to deno and module configuration

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-import "./import_map.json" with {type: "json"}
-
 globalThis.SERVER_ROOT = import.meta.url.replace("index.js", "")
 globalThis.SERVER_TIME = new Date()
 
 import * as server from "modules/server"
+import * as database from "modules/database"
 
-const { connect: mongoConnect } = await import("modules/database")
-
-await mongoConnect(true)
-
+await database.connect(true)
 server.start("127.0.0.1", 3001)
