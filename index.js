8defa63779f91150bff604480fb1dcfb44517230
Wiktor Owczarek <wiktor.owczarek@zhp.ie>
2025-03-22 22:25:25 +0000

    Add command line arguments for port and database

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
+// Set up globals for root directory and startup time
 globalThis.SERVER_ROOT = import.meta.url.replace("index.js", "")
 globalThis.SERVER_TIME = new Date()
 
 import * as server from "modules/server"
 import * as database from "modules/database"
+import * as cli from "jsr:@std/cli"
 
-await database.connect(true)
+// Parse command line arguments
+const args = cli.parseArgs(Deno.args, {
+	boolean: ["clear-database"],
+	string: ["host", "port", "db"]
+})
 
-if(Deno.args[0] == "--clear-database") {
+// Connect to database
+await database.connect(args.db)
+
+// Clear database functionality
+if(args["clear-database"]) {
 	await database.clear()
 	Deno.exit()
 }
 
+// Setup database ()
 await database.setup()
 
-server.start("127.0.0.1", 3001)
+// Start server
+server.start(args.host, args.port)
 
+// Handle errors in async functions used without await
 globalThis.addEventListener("unhandledrejection", event => {
 	event.preventDefault()
 	console.log("\n\x1b[91m[Critical]\x1b[0m UNHANDLED REJECTION!")
