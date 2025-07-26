3a58b6a7f55f0ae8fb3f354e1bceba57cb759294
Wiktor Owczarek <wiktor.owczarek@zhp.ie>
2025-07-26 21:10:45 +0100

    Automatically remove spaces from access code during login

--- public/js/login/main.js
+++ public/js/login/main.js
@@ -1,4 +1,8 @@
 API.registerHandler("login", {
 	progressText: "Logowanie...",
+	validate: data => {
+		data.accessCode = data.accessCode.replaceAll(" ", "")
+		return !!data.accessCode
+	},
 	after: () => document.location.reload()
 })
