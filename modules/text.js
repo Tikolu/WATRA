d44a1b692e61fb85bc1bdcbdc204a504a6e2b3c8
Wiktor Owczarek <wiktor.owczarek@zhp.ie>
2025-09-23 14:44:21 +0100

    Add Text.id helper function

--- modules/text.js
+++ modules/text.js
@@ -26,7 +26,26 @@ export function formatName(name) {
 		// Remove any characters that are not letters, hyphens, apostrophes or spaces
 		.replace(/[^\p{L}'\- \s]/gu, '')
 		// Capitalise the first letter of each word
-		.replace(/(\p{L})(\p{L}*)/gu, (_, p1, p2) => p1.toUpperCase() + p2.toLowerCase())
+		.replace(/(\p{Letter})(\p{Letter}*)/gu, (_, p1, p2) => p1.toUpperCase() + p2.toLowerCase())
 		// Remove hyphens and apostrophes from start and end
 		.replace(/^[\'\-]+|[\'\-]+$/g, '')
+}
+
+/**
+ * Converts a string to a valid ID. Lowercases the text, replaces spaces with
+ * hyphens and removes any characters that are not letters, numbers or hyphens
+ * @param {string} text
+ * @returns {string} ID
+ */
+export function id(text) {
+	if(!text || typeof text != "string") return ""
+	return text
+		// Remove spaces from start and end
+		.trim()
+		// Lowercase
+		.toLowerCase()
+		// Replace spaces with hyphens
+		.replace(/\s+/g, "-")
+		// Remove any characters that are not letters, numbers or hyphens
+		.replace(/[^\p{Lowercase_Letter}0-9\-]/gu, "")
 }
