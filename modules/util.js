df826ade1ffb0dac37a2013469afdbd4fa45dbe5
Wiktor Owczarek <wiktor.owczarek@zhp.ie>
2025-10-03 20:00:44 +0100

    Add array helper methods (conditional and unique)

--- modules/util.js
+++ modules/util.js
@@ -7,3 +7,32 @@ Object.isEmpty = object => {
 	}
 	return false
 }
+
+/** Function for creating an array from conditional pairs */
+Array.conditional = (...values) => {
+	const result = []
+	for(let i = 0; i < values.length; i += 2) {
+		if(!values[i]) continue
+		result.push(values[i + 1])
+	}
+	return result
+}
+
+/** Function for getting unique items from an array */
+Object.defineProperty(Array.prototype, "unique", {
+	value: function(key) {
+		const output = []
+		for(const item of this) {
+			if(key === undefined) {
+				if(output.includes(item)) {
+					continue
+				}
+			} else if(output.find(i => i[key] === item[key])) {
+				continue
+			}
+			output.push(item)
+		}
+
+		return output
+	}
+})
