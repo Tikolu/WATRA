9547562464cbb56c37bf2f4257280fd4a0033704
Wiktor Owczarek <wiktor.owczarek@zhp.ie>
2025-11-20 15:23:06 +0000

    Unit bug fixes

--- routes/api/unit/[unitID]/subUnit/create.js
+++ routes/api/unit/[unitID]/subUnit/create.js
@@ -4,14 +4,14 @@ import Unit from "modules/schemas/unit";
 export default async function({user, targetUnit, type, name="", org}) {
 	// Check permissions
 	await user.requirePermission(targetUnit.PERMISSIONS.ADD_SUBUNIT)
-	
-	// Create unit, calculating type if not specified
-	const subUnit = new Unit({
-		type: type || (targetUnit.type - 1),
-		name,
-		org
-	})
 
+	if(!org) org = undefined
+	
+	// Create unit
+	const subUnit = new Unit({type, name, org})
+	await subUnit.validate()
+
+	// Add unit to upper unit
 	await targetUnit.addSubUnit(subUnit)
 
 	// Save subUnit to DB
