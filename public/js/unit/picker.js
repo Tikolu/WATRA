125b550424e763d30e04a6a763bb6466068c4306
Wiktor Owczarek <wiktor.owczarek@zhp.ie>
2026-02-14 20:10:03 +0000

    User and unit picker styling changes

--- public/js/unit/picker.js
+++ public/js/unit/picker.js
@@ -1,9 +1,19 @@
 for(const unitPicker of document.querySelectorAll(".unit-picker")) {
+	const chooseAll = document.getElementById(`${unitPicker.id}-choose-all`)
+	chooseAll?.globalCheckbox(unitPicker)
+
 	// Open all groups of selected units
 	const selected = unitPicker.querySelectorAll("input[type=checkbox]:checked")
 	for(const checkbox of selected) {
 		for(const group of checkbox.parentElementChain) {
 			if(group.tagName === "DETAILS") group.open = true
+		}
+	}
+
+	// Update checkbox when group opens or closes
+	for(const group of unitPicker.querySelectorAll("details")) {
+		group.ontoggle = () => {
+			chooseAll?.calculateState()
 		}
 	}
 }
