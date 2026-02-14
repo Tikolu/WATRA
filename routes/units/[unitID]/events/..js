c24575ac9d854bd5eec385b595be0c4776ab2ec8
Wiktor Owczarek <wiktor.owczarek@zhp.ie>
2026-02-14 00:18:03 +0000

    Change permissions for event list page

--- routes/units/[unitID]/events/..js
+++ routes/units/[unitID]/events/..js
@@ -1,27 +1,23 @@
 import html from "modules/html.js"
 
 export default async function({user, targetUnit}) {
-	await user.requirePermission(targetUnit.PERMISSIONS.ACCESS_MEMBERS)
+	await user.requirePermission(targetUnit.PERMISSIONS.ACCESS_EVENTS)
 	
 	// Load all events
 	await targetUnit.populate("events")
 
 	// Load all event invites
-	if(await user.checkPermission(targetUnit.PERMISSIONS.MANAGE_INVITES)) {
-		await targetUnit.populate("eventInvites")
-	}
+	await targetUnit.populate("eventInvites")
 
 	// Load all subUnit events
 	const subUnitEvents = []
-	if(await user.hasRoleInUnits("manageEvent", targetUnit)) {
-		subUnitEvents.push(...await Array.fromAsync(targetUnit.getSubUnitEvents()))
-		// Sort by date
-		subUnitEvents.sort((a, b) => {
-			const aDate = a.dates.start || 0
-			const bDate = b.dates.start || 0
-			return aDate - bDate
-		})
-	}
+	subUnitEvents.push(...await targetUnit.listEvents().toArray())
+	// Sort by date
+	subUnitEvents.sort((a, b) => {
+		const aDate = a.dates.start || 0
+		const bDate = b.dates.start || 0
+		return aDate - bDate
+	})
 
 	return html("unit/eventList", {
 		user,
